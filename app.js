3396d6436e6eaca2a0f1f4ea448c883b6751284a
thuytran <tranphuongthuy97@gmail.com>
2023-03-09 11:01:09 +0700

    campgrounds crud

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
 const Campground = require("./models/campground");
+const methodOverride = require("method-override");
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -15,18 +16,59 @@ main()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/make-campground", (req, res) => {
-  const camp = new Campground({
-    title: "My backyard",
-    description: "First campground",
-    price: "$0",
-  });
-  camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index.ejs", { campgrounds });
 });
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new.ejs");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show.ejs", { campground });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const { campground } = req.body;
+  await Campground.create(campground);
+  res.redirect("/");
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { campground } = req.body;
+  await Campground.findByIdAndUpdate(req.params.id, { ...campground });
+  res.redirect(`/campgrounds/${req.params.id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
+
+// app.get("/make-campground", (req, res) => {
+//   const camp = new Campground({
+//     title: "My backyard",
+//     description: "First campground",
+//     price: "$0",
+//   });
+//   camp.save();
+//   res.send(camp);
+// });
 
 app.listen(3000, () => console.log("Serving on port 3000"));
