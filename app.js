a32d89750eb9306c9095b2f94b4094b42063387f
thuytran <tranphuongthuy97@gmail.com>
2023-03-16 15:57:31 +0700

    Added Reviews Model

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const ejsMate = require("ejs-mate");
 const app = express();
 const path = require("path");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 const methodOverride = require("method-override");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
-const { campgroundSchema } = require("./schemas");
+const { campgroundSchema, reviewSchema } = require("./schemas");
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
@@ -37,6 +37,14 @@ const validateCampground = (req, res, next) => {
   } else next();
 };
 
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else next();
+};
+
 app.get(
   "/campgrounds",
   catchAsync(async (req, res) => {
@@ -53,7 +61,7 @@ app.get(
   "/campgrounds/:id",
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
+    const campground = await Campground.findById(id).populate("reviews");
     res.render("campgrounds/show.ejs", { campground });
   })
 );
@@ -95,6 +103,22 @@ app.delete(
     res.redirect("/campgrounds");
   })
 );
+
+app.post("/campgrounds/:id/reviews", validateReview, async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id/reviews/:reviewId", async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+});
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 
 const CampgroundSchema = new mongoose.Schema({
   title: String,
@@ -6,6 +7,20 @@ const CampgroundSchema = new mongoose.Schema({
   imageUrl: String,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+CampgroundSchema.post("findOneAndDelete", async (doc) => {
+  if (doc.reviews) {
+    await Review.deleteMany({
+      _id: { $in: doc.reviews },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
